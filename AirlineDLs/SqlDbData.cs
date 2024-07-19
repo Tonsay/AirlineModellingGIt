20a55b8d946eef0986f04fed3d74f71a28580a18
Florence Pearl Tonsay <162805757+Tonsay@users.noreply.github.com>
2024-07-19 19:09:28 +0800

    Add files via upload

---
+++ AirlineDLs/SqlDbData.cs
@@ -0,0 +1,111 @@
+﻿using AirlineModelling;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineDLs
+{
+    public class SqlDbData
+    {
+        static string connectionString
+             // = "Data Source = PEARL-LAPTOP\\SQLEXPRESS;  Initial Catalog = AirlineModelling; Integrated Security = True;"; 
+             = "Server = tcp: 20.198.228.121; Database=AirlineModelling; User Id = sa; Password=Tonsay123!;";
+       
+        static SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+        public static void Connect()
+        {
+            sqlConnection.Open();
+        }
+
+        public List<Booking> GetDetails()
+        {
+            string selectStatement = "SELECT ticketnum,usernames,status FROM Details";
+
+            SqlCommand selectCommand = new SqlCommand(selectStatement, sqlConnection);
+
+            sqlConnection.Open();
+            List<Booking> users = new List<Booking>();
+
+            SqlDataReader reader = selectCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+            
+                string ticketnum = reader["ticketnum"].ToString();
+                string user = reader["usernames"].ToString();
+                
+                Booking readUser = new Booking();
+                readUser.ticketnum = ticketnum;
+                readUser.user = user;
+                readUser.status = Convert.ToInt32(reader["status"]);
+
+                users.Add(readUser);
+            }
+
+            sqlConnection.Close();
+
+            return users;
+        }
+        public int AddUser(string ticketnum, string user)
+        {
+            int success;
+
+            string insertStatement = "INSERT INTO Details VALUES (@ticketnum,@usernames)";
+
+            SqlCommand insertCommand = new SqlCommand(insertStatement, sqlConnection);
+
+            insertCommand.Parameters.AddWithValue("@usernames", user);
+            insertCommand.Parameters.AddWithValue("@ticketnum", ticketnum);
+            insertCommand.Parameters.AddWithValue("@status", 1);
+            sqlConnection.Open();
+
+            success = insertCommand.ExecuteNonQuery();
+
+            sqlConnection.Close();
+
+            return success;
+
+        }
+
+        public int UpdateUser(string ticketnum, string user)
+        {
+            int success;
+
+            string updateStatement = $"UPDATE Details SET usernames = @usernames WHERE ticketnum = @ticketnum";
+
+            SqlCommand updateCommand = new SqlCommand(updateStatement, sqlConnection);
+
+            sqlConnection.Open();
+
+            updateCommand.Parameters.AddWithValue("@usernames", user);
+            updateCommand.Parameters.AddWithValue("@ticketnum", ticketnum);
+
+            success = updateCommand.ExecuteNonQuery();
+
+            sqlConnection.Close();
+
+            return success;
+
+        }
+
+        public int DeleteUser(string ticketnum)
+        {
+            int success;
+            string deleteStatement = $"DELETE FROM Details WHERE ticketnum = @ticketnum";
+            SqlCommand deleteCommand = new SqlCommand(deleteStatement, sqlConnection);
+            sqlConnection.Open();
+
+            deleteCommand.Parameters.AddWithValue("@ticketnum", ticketnum);
+
+            success = deleteCommand.ExecuteNonQuery();
+
+            sqlConnection.Close();
+            return success; 
+        }
+
+    }
+}
---
+++ AirlineDLs/UserData.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using AirlineModelling;
+
+namespace AirlineDLs
+{
+    public class UserData
+    {
+
+        List<Booking> users;
+        SqlDbData sqlData;
+        
+        public UserData()
+        {
+            users = new List<Booking>();
+            sqlData = new SqlDbData();
+
+            //InfoDL _userFactory = new InfoDl();
+            //users = _userFactory.GetDummysDetail();
+        }
+
+        public List<Booking> GetDetails()
+        {
+           users = sqlData.GetDetails();
+           return users;
+        }
+
+        public int AddUser(Booking user)
+        {
+            return sqlData.AddUser(user.ticketnum, user.user);
+        }
+
+        public int UpdateUser(Booking user)
+        {
+            return sqlData.UpdateUser(user.ticketnum, user.user);
+        }
+
+        public int DeleteUser(Booking user)
+        {
+            return sqlData.DeleteUser(user.ticketnum);
+        }
+    }
+}
